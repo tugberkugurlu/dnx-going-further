1353f2ddd251b1ddca83f8968ba0d74bcee17e47
tugberkugurlu <tugberkugurlu@gmail.com>
2015-06-30 07:51:41 +0100

    added AnalyzersCompileModule

---
+++ src/AnalyzersCompileModule/compiler/preprocess/AnalyzersCompileModule.cs
@@ -0,0 +1,34 @@
+using System.Collections.Immutable;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.Framework.Runtime;
+using Microsoft.Framework.Runtime.Roslyn;
+
+public class AnalyzersCompileModule : ICompileModule
+{
+    private readonly string _basePath;
+
+    public AnalyzersCompileModule(IApplicationEnvironment appEnv)
+    {
+        _basePath = appEnv.ApplicationBasePath;
+    }
+
+    public void BeforeCompile(IBeforeCompileContext context)
+    {
+        string analyzerAssemblyPath = Path.Combine(_basePath, @"lib/DotNetDoodle.Analyzers.dll");
+        ImmutableArray<DiagnosticAnalyzer> diagnosticAnalyzers = new AnalyzerFileReference(analyzerAssemblyPath).GetAnalyzers(LanguageNames.CSharp);
+        var compilation = context.Compilation.WithAnalyzers(diagnosticAnalyzers);
+        ImmutableArray<Diagnostic> diagsnostics = compilation.GetAnalyzerDiagnosticsAsync().Result;
+
+        foreach (var diagsnostic in diagsnostics)
+        {
+            context.Diagnostics.Add(diagsnostic);
+        }
+    }
+
+    public void AfterCompile(IAfterCompileContext context)
+    {
+    }
+}
